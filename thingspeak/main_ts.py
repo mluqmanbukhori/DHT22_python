8ae742006557accf3a39ea557fa532007a77ab45
Muhammad Luqman Bukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2022-04-20 23:54:21 +0700

    Update main_ts.py

--- thingspeak/main_ts.py
+++ thingspeak/main_ts.py
@@ -4,34 +4,31 @@ import adafruit_dht
 from urllib.request import urlopen
 
 dhtDevice = adafruit_dht.DHT22(board.D18)
-API = "C9JMA5HJZLTULA2X" # ganti dengan API kalian
-URL = "https://api.thingspeak.com/update?api_key={}".format(API)
+API_KEY = "H7O3MY7QV2RRMZ29"
+URL = "https://api.thingspeak.com/update?api_key={}".format(API_KEY)
 
-SensorTimePrev  = 0
-SensorInterval  = 2 # 2 detik
-ThingSpeakTimePre   = 0
-ThingSpeakInterval  = 20 #detik
+interval = 20 # 20 detik
+SensorTime = 0
+TsTime = 0
 
 try:
     while True:
-        if time() - SensorTimePrev > SensorInterval:
-            SensorTimePrev = time()
-
+        if time() - SensorTime > interval:
+            SensorTime = time()
             temperature = dhtDevice.temperature
             humidity = dhtDevice.humidity
             print("Temp: {:.1f}*C\tHumidity: {}% ".format(temperature, humidity))
-
-        if time() - ThingSpeakTimePre > ThingSpeakInterval:
-            ThingSpeakTimePre = time()
-
-            tsHttp = URL + "&field1={:.2f}&field2={:.2f}".format(temperature, humidity)
+        
+        if time() - TsTime > interval:
+            TsTime = time()
+            
+            tsHttp = URL + "&field1={:.2f}&field2={:.2f}".format(temperature,humidity)
             print(tsHttp)
-
             conn = urlopen(tsHttp)
             print("Response: {}".format(conn.read()))
+            print("tunggu time release: {} detik".format(interval))
             conn.close()
-            sleep(1)
-
+        
 except KeyboardInterrupt:
     conn.close()
     print("[INFO] quit program...")
