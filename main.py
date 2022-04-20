bbcf7ac6a1e255af1cc5864606f579e56354fef9
Muhammad Luqman Bukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2022-04-20 20:44:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import board
 import adafruit_dht
 
 dhtDevice = adafruit_dht.DHT22(board.D18)
- 
-while True:
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
-    print("Temp: {:.1f} C    Humidity: {}% ".format(temperature, humidity))
-    time.sleep(1) 
+
+try:
+    while True:
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f}*C\tHumidity: {}% ".format(temperature, humidity))
+        time.sleep(2)
+        
+except KeyboardInterrupt:
+    print("[INFO] quit program...")
